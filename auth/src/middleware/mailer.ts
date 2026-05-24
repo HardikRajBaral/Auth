0ddcc95e5c168bd5449cfff1ffe6e873300d1cc6
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-05-24 22:15:36 +0545

    refactor: Remove unnecessary 'peer' property from package-lock.json and update mailer transport configuration

--- auth/src/middleware/mailer.ts
+++ auth/src/middleware/mailer.ts
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer"
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // Use true for port 465, false for port 587
+  secure: false, 
   auth: {
     user: "hardikrajbaral232@gmail.com",
     pass: process.env.GOOGLE_CLIENT_ID,
